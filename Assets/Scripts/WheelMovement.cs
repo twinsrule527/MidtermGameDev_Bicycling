0d9b298e16582329c7d2e4d08a01b26b2441facd
Eli <esc487@nyu.edu>
2020-10-02 14:52:24 -0500

    First things made
    Includes: Parent bike, as well as two collider bike objects; basic player controls, with bike-like turning and movement

---
+++ Assets/Scripts/WheelMovement.cs
@@ -0,0 +1,63 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+//PURPOSE: Used for the wheel's actual movement: declares the player's velocity, as well as moves them
+//USAGE: Attached to the player's front wheel
+public class WheelMovement : MonoBehaviour
+{
+    //Neeed to have a force perpendicular to the center of the entire bike considered as a particle
+    //Perpendicular is proportional to speed^2 (inversely proportional to radius of curvature)
+    //Needed to determine force: angle between wheel and frame; speed of wheel
+    
+    public float max_speed;
+    //Used to determine the max speed the player can go at (speed is approx. 1/2 what it would be in mph)
+    Vector3 velocity;
+    //Very simple, just added to the player's position every frame to determine where they are going
+    public float speed;
+    //At any given moment, this is the speed at which the player is moving
+    public float acceleration;
+    //Is used to determine the rate at which speed changes
+    public float deacceleration;
+    //Rate at which a player may slow down voluntary; faster than acceleration because of brakes
+    public float turn_radius;
+    //The constant for the radius around which the player turns when they are turning (may need tweaks)
+    public Transform Directional;
+    //The player's handlebars, determines direction they are going in
+    Transform myTransform;
+    //The transform of this object, used for player's movement
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        //These two floats determine the direction the player's wheel is pointing
+        float y_direction = Mathf.Sin(( Directional.eulerAngles.z * Mathf.PI) / 180 );
+        float x_direction = Mathf.Cos(( Directional.eulerAngles.z * Mathf.PI) / 180 );
+        if(Input.GetKey(KeyCode.W)) {
+            //The player presses W to accelerate
+           speed += acceleration*Time.deltaTime;
+           if( speed > max_speed ) {
+               speed = max_speed;
+           }
+        }
+        if(Input.GetKey( KeyCode.S )) {
+            speed +=deacceleration*Time.deltaTime;
+            if(speed < 0 ) {
+                speed = 0;
+            }
+        }
+        //The player will always move forward, as long as speed > 0
+        velocity = new Vector3(x_direction,y_direction,0f) * Time.deltaTime * speed;
+        myTransform.position += velocity;
+        //If the player is turning, their bike will turn relative to how much they turn by
+        if(Directional.localEulerAngles.z != 90) {
+            float turning = turn_radius * ( 30 / Directional.localEulerAngles.z - 90);
+            myTransform.localEulerAngles += new Vector3(0f, 0f, turning)*speed*speed*Time.deltaTime;
+        }  
+    }
+}
---
+++ Assets/Scripts/WheelRotation.cs
@@ -0,0 +1,53 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PURPOSE: Rotates the player's front wheel, allowing for the player to turn.
+//USAGE: Attached to the front wheel sprite, which is "attached" to the player's frame sprite
+
+public class WheelRotation : MonoBehaviour
+{
+    Transform myTransform;
+    float wheel_rotate;
+    public float rotation_speed;
+    public float max_rotate;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myTransform = GetComponent<Transform>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        wheel_rotate = myTransform.localEulerAngles.z;
+        //Debug.Log(wheel_rotate.ToString());
+        //The first press the player makes is less responsive, allowing for easier more slight movements
+        if(Input.GetKeyDown(KeyCode.A)) {
+            //If the wheel is not at its maximum rotation, it will rotate
+            if(wheel_rotate < 90 + max_rotate) {
+                Vector3 rotation_amt = new Vector3(0f, 0f, rotation_speed*Time.deltaTime/2);
+                myTransform.localEulerAngles += rotation_amt;      
+            }
+        }
+        else if(Input.GetKey(KeyCode.A) ) {
+            //If the player is not pointed all the way to the left
+            if(wheel_rotate < 90 + max_rotate) {
+                Vector3 rotation_amt = new Vector3(0f, 0f, rotation_speed*Time.deltaTime);
+                myTransform.localEulerAngles += rotation_amt;
+            }
+        }
+        if(Input.GetKeyDown(KeyCode.D)) {
+            if(wheel_rotate > 90 - max_rotate) {
+                Vector3 rotation_amt = new Vector3(0f, 0f, -rotation_speed*Time.deltaTime/2);
+                myTransform.localEulerAngles += rotation_amt;      
+            }
+        }
+        else if(Input.GetKey( KeyCode.D ) ) {
+            if(wheel_rotate > 90 - max_rotate) {
+                Vector3 rotation_amt = new Vector3(0f, 0f, -rotation_speed*Time.deltaTime);
+                myTransform.localEulerAngles += rotation_amt;
+            }
+        }
+    }
+}
