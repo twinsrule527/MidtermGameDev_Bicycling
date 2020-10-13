03f646ccf82f9e3431470bf0d48bcf0e182395c8
Eli <esc487@nyu.edu>
2020-10-13 17:37:39 -0500

    Player look back, changed NPC movement
    Added ability for NPCs to speed up to stay ahead of each other, and player can press 'Q' to change their camera position

---
+++ Assets/Scripts/Camera_LookBack.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+//PURPOSE: Allows the player to look behind them by pressing 'Q' (the specific key might change later)
+//USAGE: Attached to the player's parent bike
+public class Camera_LookBack : MonoBehaviour
+{
+    public Transform myCamera;
+    Vector3 base_pos;
+    //The base position the camera should return to
+    float pos_change;
+    public float max_pos_change;
+    //The maximum amount the camera should be able to change by
+
+    void Start() {
+        base_pos = myCamera.localPosition;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(Input.GetKey(KeyCode.Q)) {
+            if(pos_change < max_pos_change) {
+                pos_change += Time.deltaTime * 2f;
+                if(pos_change > max_pos_change) {
+                    pos_change = max_pos_change;
+                }
+            }
+        }
+        else if(pos_change > 0f) {
+            pos_change -=Time.deltaTime * 3f;
+        }
+        else {
+            pos_change =0f;
+        }
+        myCamera.localPosition = base_pos -myCamera.right * pos_change;
+    }
+}
--- Assets/Scripts/NPC_BikeMovement_2.cs
+++ Assets/Scripts/NPC_BikeMovement_2.cs
@@ -13,11 +13,16 @@ public class NPC_BikeMovement_2 : MonoBehaviour
     public float lane_min;
     public float lane_max;
     //These determines where the bike should be horizontally relative to the wall
+    public float passing_dist;
+    //This determines the distance a bike should be from another if attempting to pass that other bike
+    bool passing;
     public float RVision0_dist;
     public float Vision0_dist;
     BoxCollider2D myCollider;
     Ray2D[] FrontRay = new Ray2D[3];
     public WheelMovement IsPaused;
+    Vector2 ref_ray_pos;
+    //used to reference the current position of a person in front of this bike, to determine if they need to move to a different lane
 
     void Start() {
         myCollider = GetComponent<BoxCollider2D>();
@@ -44,14 +49,48 @@ public class NPC_BikeMovement_2 : MonoBehaviour
         RaycastHit2D RHit1 = Physics2D.Raycast(RVision1.origin, RVision1.direction, RVision0_dist);
         Debug.DrawLine(RHit0.point, RHit1.point, Color.magenta);
         RaycastHit2D ForwardHit = Physics2D.Raycast(FrontRay[2].origin, FrontRay[2].direction, Vision0_dist);
+        //A ray behind the NPC will speed them up if an object is right behind them
+        Ray2D[] BackRay = new Ray2D[3];
+        BackRay[0] = new Ray2D(transform.position, -transform.up);
+        BackRay[1] = new Ray2D(transform.position + transform.right * 0.5f, -transform.up);
+        BackRay[2] = new Ray2D(transform.position - transform.right * 0.5f, -transform.up);
+        float BackRay_dist = 6f;
+        bool accelerate = false;
+        for(int i = 0; i<3; i++) {
+            Debug.DrawRay(BackRay[i].origin, BackRay[i].direction*BackRay_dist, Color.magenta);
+            RaycastHit2D BackHit = Physics2D.Raycast(BackRay[i].origin, BackRay[i].direction * BackRay_dist);
+            if(BackHit.collider != null && BackHit.collider.CompareTag("Vehicle")) {
+                accelerate = true;
+            }
+        }
+        if(accelerate && base_speed > 0) {
+            speed += 5*Time.deltaTime;
+        }
+        else {
+            if(speed > base_speed) {
+                speed -= 5 * Time.deltaTime;
+            }
+        }
         //If there is an object in front of the player, they may switch lanes
         if(ForwardHit.collider != null) {
-            if(ForwardHit.collider.CompareTag("Vehicle") || ForwardHit.collider.CompareTag("Player")) {
+            if(ForwardHit.collider.CompareTag("Vehicle")) {// || ForwardHit.collider.CompareTag("Player")) {
+                //Calculate a comparative speed of an object in front of this bike
+                float comp_speed = Vector3.Magnitude(ForwardHit.point - ref_ray_pos);
+                ref_ray_pos = ForwardHit.point;
+                if(comp_speed < speed) {
+                    //If the other object's speed is slower than this object, this object will attempt to pass it
+                    //if(!passing) {
+                       // passing = true;
+                      //  RaycastHit2D PassHitR = Physics2D.Raycast(ForwardHit.collider.transform.position, ForwardHit.collider.transform.right * RVision0_dist);
+                      //  lane_min += PassHitR.fraction * RVision0_dist;
+                     //   lane_max += PassHitR.fraction * RVision0_dist;
+                  //  }
+                }
                 //Want to include this later if I can, where they move over to go around, but not in current spot
                 //lane_min = 3.5f;
                 //lane_max = 4f;
                 //Currently bikes will slow down when coming upon each other, rather than attempting to pass each other
-                speed -= 1 *Time.deltaTime;
+                //speed -= 1 *Time.deltaTime;
             }
             else {
                 if( speed < base_speed ) {
